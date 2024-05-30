ff1c54e6cadbad40b0412729139b3ae22e9228ee
YinCheng0106 <eason010630@gmail.com>
2024-05-30 23:24:29 +0800

    6

--- dart/b_01.dart
+++ dart/b_01.dart
@@ -1,7 +1,14 @@
 void main(){
+  /* var 賦值後不得修改 */
   var a = 1; // int 整數
   var b = "1"; // String 字串
   var c = 1.1; // double 浮點數
+
+  /* dynamic 賦值後可修改 */
+  dynamic d = 1;
+  d = 100; // no err
+
+  /* var 如未賦值會給予變數 dynamic */
 
   if(a is int){
     print('a is int'); // Output：a is int
