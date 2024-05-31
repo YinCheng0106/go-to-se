837ff9046fb2b6b52051709e251a33f9b846e6e2
YinCheng <eason010630@gmail.com>
2024-05-31 09:06:17 +0800

    11

--- dart/b_05.dart
+++ dart/b_05.dart
@@ -1,3 +1,11 @@
 void main(){
-  
+  /* if-else */
+  var a = 5;
+  if(a > 10){
+    print("a 大於 10");
+  } else if(a <= 10 && a >= 0){
+    print("a 介於 0 到 10 之間");
+  } else {
+    print("a 小於 0");
+  }
 }
