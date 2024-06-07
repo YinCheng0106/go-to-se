8635bf2deb6fd79f57d9a9a4417185e4f4c7c9b1
YinCheng <eason010630@gmail.com>
2024-06-07 09:37:29 +0800

    18

--- dart/b_10.dart
+++ dart/b_10.dart
@@ -5,11 +5,14 @@ void main(){
 }
 
 class Video{
-  String title;
-  String description;
-  String publishTime;
-  String thumbnail;
-  String url;
+  //非空安全（null safety）功能
+  //明確地允許它們為 null，否則會報錯
+  //可以在宣告類別前方加入late
+  late String title;
+  late String description;``
+  late String publishTime;
+  late String thumbnail;
+  late String url;
 
   Video(String title, String description, String publishTime, String thumbnail, String url){
     this.title = title;
