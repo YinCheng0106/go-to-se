720670027cd8d718804d3f8de5b2fb7dfc59e6a2
YinCheng <eason010630@gmail.com>
2024-06-09 00:13:41 +0800

    23

--- dart/b_13.dart
+++ dart/b_13.dart
@@ -1,6 +1,10 @@
+/* Async 非同步 */
+
 import 'dart:async';
 
-Future<void> introduction(){
+//Future 是一個 class，用來表示非同步操作的結果
+//擁有兩個狀態，完成(completed)/未完成(uncompleted)
+Future <void> introduction(){
   return Future.delayed(
     Duration(seconds: 2), () => print("Dart tutorial")
   );
