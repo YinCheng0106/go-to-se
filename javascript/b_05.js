1c953e60bd0d0261a7a31841ed526c7c99ef2a82
YinCheng <eason010630@gmail.com>
2024-06-09 23:09:18 +0800

    30

---
+++ javascript/b_05.js
@@ -0,0 +1,28 @@
+/* while loop */
+
+/*
+while(condition){
+    code
+    }
+ */
+let i = 0;
+let text = "";
+while(i < 10){
+    text += "The number is " + i + "\n";
+    i++;
+}
+console.log(text);
+/* 
+Output:
+    The number is 0
+    The number is 1
+    The number is 2
+    The number is 3
+    The number is 4
+    The number is 5
+    The number is 6
+    The number is 7
+    The number is 8
+    The number is 9
+    
+ */
