915fc9a70d2880c11c1a25428bc13c214b0bdbb7
YinCheng0106 <eason010630@gmail.com>
2024-06-12 23:04:23 +0800

    37

--- javascript/b_11.js
+++ javascript/b_11.js
@@ -11,8 +11,60 @@ fruits.set("a", 5);
 fruits.set("b", 3);
 fruits.set("c", 2);
 
+// forEach() 
+let text = "";
+fruits.forEach (function(value, key) {
+    text += key + ' = ' + value;
+});
+console.log(text); // Output: apples = 500banana = 300oranges = 200a = 5b = 3c = 2
+
+// entries()
+let txt = "";
+for(const x of fruits.entries()){
+    txt += x;
+}
+console.log(txt); //Output: apples,500banana,300oranges,200a,5b,3c,2
+
+// keys()
+let t = "";
+for(const x of fruits.keys()){
+    t += x;
+}
+console.log(t); //Output: applesbananaorangesabc
+
+// values()
+let tx = "";
+for(const x of fruits.values()){
+    tx += x;
+}
+console.log(tx); //Output: 500300200532
+
 // get() 可以取得Map中key的值
 console.log(fruits.get("apples"));// Output: 500
 
 // map 是一個 Objects
 console.log(typeof fruits); // Output: object
+
+// .size 取得Map的大小
+console.log("fruits size of " + fruits.size); // Output: fruits size of 6
+
+// delete() 刪除元素
+fruits.delete("apples");
+console.log(fruits);
+/* Output:
+Map(5) {
+    'banana' => 300,
+    'oranges' => 200,
+    'a' => 5,
+    'b' => 3,
+    'c' => 2
+}
+*/
+
+// clear() 清除Map所有的元素
+fruits.clear();
+console.log(fruits); // Output: Map(0) {}
+
+// has() 是否有此元素
+console.log(fruits.has("banana")); //Output: flase
+
