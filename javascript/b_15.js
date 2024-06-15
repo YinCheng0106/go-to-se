72eff31f2df3df1cd98899fc65f551be7ce3313f
YinCheng0106 <eason010630@gmail.com>
2024-06-15 23:52:46 +0800

    43

--- javascript/b_15.js
+++ javascript/b_15.js
@@ -16,3 +16,14 @@ console.log(z);
 let m = ~5; // 11111111111111111111111111111010 (-6)
 console.log(m);
 
+// << Bitwise Left Shift
+let n = 5 << 1; // 00000000000000000000000000001010 (10)
+console.log(n);
+
+// >> Bitwise Right Shift
+let p = -5 >> 1; // 11111111111111111111111111111101 (-3)
+console.log(p);
+
+// >>> Right Shift
+let q = 5 >>> 1; // 00000000000000000000000000000010 (2)
+console.log(q);
