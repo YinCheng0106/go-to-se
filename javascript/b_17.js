df3e79364b4a4e152da598879fca0da3950958a7
YinCheng0106 <eason010630@gmail.com>
2024-06-16 00:36:20 +0800

    45

---
+++ javascript/b_17.js
@@ -0,0 +1,9 @@
+/* JSON */
+
+let text = '{ "employees" : [' +
+'{ "firstName":"John" , "lastName":"Doe" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" },' +
+'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+
+const obj = JSON.parse(text);
+console.log(obj);
