dac81233e396197bde1187d1bb4647bc39f743c4
YinCheng0106 <eason010630@gmail.com>
2024-06-15 23:31:30 +0800

    42

---
+++ javascript/b_15.js
@@ -0,0 +1,18 @@
+/* Bitwise Operations */
+
+// & AND
+let x = 5 & 1; // 0001 (1)
+console.log(x);
+
+// | OR
+let y = 5 | 1; // 0101 (5)
+console.log(y);
+
+// ^ XOR
+let z = 5 ^ 1; // 0100 (4)
+console.log(z);
+
+// ~ NOT
+let m = ~5; // 11111111111111111111111111111010 (-6)
+console.log(m);
+
