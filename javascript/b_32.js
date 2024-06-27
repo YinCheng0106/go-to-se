94951fe94436a5ca767e51d9ad942fc8f8547f6a
YinCheng0106 <eason010630@gmail.com>
2024-06-27 21:08:04 +0800

    62

---
+++ javascript/b_32.js
@@ -0,0 +1,18 @@
+/* Function bind() */
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    fullName: function(){
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+const member = {
+    firstName: "Hege",
+    lastName: "Nilsen",
+}
+
+person.fullName; // undefined
+let fullName = person.fullName.bind(member); // "Hege Nilsen"
+
