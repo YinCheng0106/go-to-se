c26fce9897c309b1cd1464769e81d8dd37f6dd40
YinCheng0106 <eason010630@gmail.com>
2024-07-01 00:15:39 +0800

    75

---
+++ javascript/b_46.js
@@ -0,0 +1,11 @@
+/* Animation */
+
+// id = setInterval(frame, 5);
+
+// function frame() {
+//     if(/* test for finished */){
+//         clearInterval(id);
+//     } else {
+//         /* code to change the element style */
+//     }
+// }
