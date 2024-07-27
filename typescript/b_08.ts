f277719b15f630c1ffa25c71ede31ee90666d1b4
YinCheng0106 <eason010630@gmail.com>
2024-07-27 23:45:31 +0800

    85

--- typescript/b_08.ts
+++ typescript/b_08.ts
@@ -27,3 +27,57 @@ const nameAgeMap: Record<string, number> = {
     'Alice' : 21,
     'Bod' : 25,
 };
+
+// Omit 忽略
+interface Person {
+    name: string;
+    age: number;
+    location?: string;
+}
+
+const bob : Omit<Person, 'age' | 'location'> = {
+    name: 'Bob',
+    // 因 Omit 刪除 age 以及 location 無法定義，只有 name 可以被定義
+}
+
+// Pick
+interface Person {
+    name: string;
+    age: number;
+    location?: string
+}
+
+const bob_1: Pick<Person, 'name'> = {
+    name: 'Bob',
+    // 因 Pick 只保留 name，所以 age 以及 location 都被刪除了，無法定義
+}
+
+// Exclude 排除
+type Primitive = string | number | boolean;
+const value: Exclude<Primitive, string> = true; // 因為已排除 string 所以在這不能使用
+
+// ReturnType 
+type PointGenerator = () => { x : number; y : number };
+const point: ReturnType<PointGenerator> = {
+    x : 10,
+    y : 20,
+}
+
+// Parameters
+type PointPrint = (p: { x: number; y: number; }) => void;
+const point_1: Parameters<PointPrint>[0] = {
+    x : 10,
+    y : 20
+}
+
+// Readonly 唯讀
+interface Person {
+    name: string;
+    age: number;
+}
+const person: Readonly<Person> = {
+    name: "Dylan",
+    age: 35,
+};
+
+person.name = 'Israel' // 因為 person 被設定為唯讀，所以不得被改變
